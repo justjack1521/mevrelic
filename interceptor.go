9dadda466f6a0600f426edbea32faf4bf0f26226
justjack1521 <henryt1521@hotmail.com>
2024-03-20 12:19:47 +1100

    added grpc interceptor

---
+++ interceptor.go
@@ -0,0 +1,28 @@
+package mevrelic
+
+import (
+	"context"
+	"github.com/newrelic/go-agent/v3/newrelic"
+	"google.golang.org/grpc"
+)
+
+func ServerInterceptor(relic *NewRelic) grpc.UnaryServerInterceptor {
+	return func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (resp interface{}, err error) {
+
+		var txn *newrelic.Transaction
+
+		if relic != nil && relic.Application != nil {
+			txn = relic.Application.StartTransaction(info.FullMethod)
+			defer txn.End()
+		}
+
+		h, err := handler(ctx, req)
+
+		if err != nil {
+			txn.NoticeError(err)
+		}
+
+		return h, err
+
+	}
+}
