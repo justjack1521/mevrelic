3df41c3f37dcb7f1fd36e74b36beca2c0059c29b
justjack1521 <henryt1521@hotmail.com>
2024-03-23 20:53:00 +1100

    Updated Command Handler interface and remove Query Handler

--- decorator.go
+++ decorator.go
@@ -9,60 +9,25 @@ type Command interface {
 	CommandName() string
 }
 
-type Query interface {
-	QueryName() string
-}
-
-type CommandHandler[C Command] interface {
-	Handle(ctx context.Context, cmd C) error
-}
-
-type QueryHandler[C Query, R any] interface {
+type CommandHandler[C Command, R any] interface {
 	Handle(ctx context.Context, cmd C) (R, error)
 }
 
-type commandHandlerWithNewRelic[C Command] struct {
+type commandHandlerWithNewRelic[C Command, R any] struct {
 	relic *newrelic.Application
-	base  CommandHandler[C]
+	base  CommandHandler[C, R]
 }
 
-func NewCommandDecoratorWithNewRelic[C Command](nrl *newrelic.Application, handler CommandHandler[C]) CommandHandler[C] {
-	return commandHandlerWithNewRelic[C]{
+func NewCommandDecoratorWithNewRelic[C Command, R any](nrl *newrelic.Application, handler CommandHandler[C, R]) CommandHandler[C, R] {
+	return commandHandlerWithNewRelic[C, R]{
 		relic: nrl,
 		base:  handler,
 	}
 }
 
-func (c commandHandlerWithNewRelic[C]) Handle(ctx context.Context, cmd C) (err error) {
+func (c commandHandlerWithNewRelic[C, R]) Handle(ctx context.Context, cmd C) (result R, err error) {
 
 	txn := c.relic.StartTransaction(cmd.CommandName())
-	var nrc = newrelic.NewContext(ctx, txn)
-
-	defer func() {
-		if err != nil {
-			txn.NoticeError(err)
-		}
-		txn.End()
-	}()
-
-	return c.base.Handle(nrc, cmd)
-}
-
-type queryHandlerWithNewRelic[C Query, R any] struct {
-	relic *newrelic.Application
-	base  QueryHandler[C, R]
-}
-
-func NewQueryDecoratorWithNewRelic[C Query, R any](nrl *newrelic.Application, handler QueryHandler[C, R]) QueryHandler[C, R] {
-	return queryHandlerWithNewRelic[C, R]{
-		relic: nrl,
-		base:  handler,
-	}
-}
-
-func (c queryHandlerWithNewRelic[C, R]) Handle(ctx context.Context, cmd C) (response R, err error) {
-
-	txn := c.relic.StartTransaction(cmd.QueryName())
 	var nrc = newrelic.NewContext(ctx, txn)
 
 	defer func() {
